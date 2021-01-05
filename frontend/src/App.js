c13ae44b86a10f1f8b564bd674a13719f6cec9d8
arianrah <arianrahimi@icloud.com>
2021-01-05 10:00:08 -0800

    more cleaning in the front

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,10 @@
 import React from 'react';
 
-function App() {
+const App = ()  => {
   return (
-    <div className="App">
-     boardgeek
-    </div>
+    <>
+     <h1>Welcome To Boardgeek</h1>
+    </>
   );
 }
 
