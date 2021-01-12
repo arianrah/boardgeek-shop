9227e93caa6d1b8105fce1f9e0d4a413ee9190db
arianrah <arianrahimi@icloud.com>
2021-01-12 09:48:37 -0800

    Barebones navbar and footer created. Brought in react-bootstrap Lux.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,20 @@
 import React from 'react';
+import { Container } from 'react-bootstrap';
+
+//components
+import Header from './components/Header'
+import Footer from './components/Footer'
 
 const App = ()  => {
   return (
     <>
-     <h1>Welcome To Boardgeek</h1>
+    <Header/>
+    <main className="py-3">
+      <Container>
+        <h1>Welcome To Boardgeek</h1>
+      </Container>
+    </main>
+    <Footer/>
     </>
   );
 }
