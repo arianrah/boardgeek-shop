4004bccf1c491b1f8eeefc4ef88b6af75b51b58e
arianrah <arianrahimi@icloud.com>
2021-02-25 17:37:19 -0800

    Loader, Messaging component completed. HomeScreen finished.

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -8,22 +8,28 @@ const router = express.Router()
 //@desc   fetch all products
 //@route  GET /api/products
 //@access public
-router.get('/', asyncHandler(async(req, res) => {
-  const products = await Product.find({})
-  res.json(products)
-}))
+router.get(
+	'/',
+	asyncHandler(async (req, res) => {
+		const products = await Product.find({})
+		res.json(products)
+	})
+)
 
 //@desc   fetch single product
 //@route  GET /api/products/:id
 //@access public
-router.get('/:id', asyncHandler(async(req, res) => {
-  const product = await Product.findById(req.params.id)
-    if(product) {
-      res.json(product)
-    } else {
-      res.status(404)
-      throw new Error('Product not found')
-    }
-}))
+router.get(
+	'/:id',
+	asyncHandler(async (req, res) => {
+		const product = await Product.findById(req.params.id)
+		if (product) {
+			res.json(product)
+		} else {
+			res.status(404)
+			throw new Error('Product not found')
+		}
+	})
+)
 
 export default router
--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -3,6 +3,8 @@ import { useDispatch, useSelector } from 'react-redux'
 import { Row, Col } from 'react-bootstrap'
 
 import Product from '../components/Product'
+import Message from '../components/Message'
+import Loader from '../components/Loader'
 
 import { listProducts } from '../actions/productActions'
 
@@ -18,9 +20,9 @@ const HomeScreen = () => {
 		<>
 			<h1>Latest Products</h1>
 			{loading ? (
-				<h3>Loading...</h3>
+				<Loader />
 			) : error ? (
-				<h3>{error}</h3>
+				<Message variant="danger">{error}</Message>
 			) : (
 				<Row>
 					{products.map((product) => (
