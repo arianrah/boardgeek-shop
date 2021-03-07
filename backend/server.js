57d5147c6190252da0e54bf5668d23cdd0490e45
arianrah <arianrahimi@icloud.com>
2021-03-07 13:38:58 -0800

    User authentication endpoint completed. Not sending JWT yet*.

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,12 @@
 import express from 'express'
+import bodyParser from 'body-parser'
 import dotenv from 'dotenv'
 import colors from 'colors'
 import connectDB from './config/db.js'
 import { notFound, errorHandler } from './middleware/errorMiddleware.js'
 
 import productRoutes from './routes/productRoutes.js'
+import userRoutes from './routes/userRoutes.js'
 
 dotenv.config()
 
@@ -12,11 +14,15 @@ connectDB()
 
 const app = express()
 
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
+
 app.get('/', (req, res) => {
-  res.send('API is running')
+	res.send('API is running')
 })
 
 app.use('/api/products', productRoutes)
+app.use('/api/users', userRoutes)
 
 app.use(notFound)
 
@@ -24,4 +30,9 @@ app.use(errorHandler)
 
 const PORT = process.env.PORT || 5555
 
-app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} on port ${PORT}`.yellow.bold))
+app.listen(
+	PORT,
+	console.log(
+		`Server running in ${process.env.NODE_ENV} on port ${PORT}`.yellow.bold
+	)
+)
