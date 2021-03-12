2897ef8a1b7e3051b021d7f5ab71692813864fe6
arianrah <arianrahimi@icloud.com>
2021-03-11 22:20:27 -0800

    Completed backend user logins and registration handling. Setup login handling for state on frontend. Login form/component not yet started.

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -12,8 +12,10 @@ const authUser = asyncHandler(async (req, res) => {
 
 	if (user && (await user.matchPassword(password))) {
 		res.json({
+			_id: user._id,
 			name: user.name,
 			email: user.email,
+			isAdmin: user.isAdmin,
 			token: generateToken(user._id),
 		})
 	} else {
@@ -41,4 +43,37 @@ const getUserProfile = asyncHandler(async (req, res) => {
 	}
 })
 
-export { authUser, getUserProfile }
+//@desc   register new user
+//@route  POST /api/users
+//@access public
+const registerUser = asyncHandler(async (req, res) => {
+	const { name, email, password } = req.body
+
+	const userExists = await User.findOne({ email })
+
+	if (userExists) {
+		res.status(400)
+		throw new Error('User already exists')
+	}
+
+	const user = await User.create({
+		name,
+		email,
+		password,
+	})
+
+	if (user) {
+		res.status(201).json({
+			_id: user._id,
+			name: user.name,
+			email: user.email,
+			isAdmin: user.isAdmin,
+			token: generateToken(user._id),
+		})
+	} else {
+		res.status(400)
+		throw new Error('Invalid user data')
+	}
+})
+
+export { authUser, getUserProfile, registerUser }
