f428bf328cf78260a8ff7543532591fa613c1a2c
arianrah <arianrahimi@icloud.com>
2021-04-13 19:31:04 -0700

    Update user profile endpoint completed.

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -2,7 +2,7 @@ import asyncHandler from 'express-async-handler'
 import generateToken from '../utils/generateToken.js'
 import User from '../models/userModel.js'
 
-//@desc   auth user (validate email/pw) + get token
+//@desc   login user
 //@route  POST /api/users/login
 //@access public
 const authUser = asyncHandler(async (req, res) => {
@@ -24,18 +24,42 @@ const authUser = asyncHandler(async (req, res) => {
 	}
 })
 
-//@desc   get user (logged in auth)
+//@desc   get user
 //@route  GET /api/users/profile
-//@access public
+//@access private
 const getUserProfile = asyncHandler(async (req, res) => {
 	const user = await User.findById(req.user._id)
-	console.log(user)
 	if (user) {
 		res.json({
 			_id: user._id,
 			name: user.name,
 			email: user.email,
 			isAdmin: user.isAdmin,
+		})
+	} else {
+		res.status(400)
+		throw new Error('User not found')
+	}
+})
+
+//@desc   update user profile
+//@route  PUT /api/users/profile
+//@access private
+const updateUserProfile = asyncHandler(async (req, res) => {
+	const user = await User.findById(req.user._id)
+	if (user) {
+		user.name = req.body.name || user.name
+		user.email = req.body.email || user.email
+		if (req.body.password) {
+			user.password = req.body.password
+		}
+		const updatedUser = await user.save()
+		res.json({
+			_id: updatedUser._id,
+			name: updatedUser.name,
+			email: updatedUser.email,
+			isAdmin: updatedUser.isAdmin,
+			token: generateToken(updatedUser._id),
 		})
 	} else {
 		res.status(400)
@@ -76,4 +100,4 @@ const registerUser = asyncHandler(async (req, res) => {
 	}
 })
 
-export { authUser, getUserProfile, registerUser }
+export { authUser, getUserProfile, registerUser, updateUserProfile }
