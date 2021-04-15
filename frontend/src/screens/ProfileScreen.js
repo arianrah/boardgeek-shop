eabad1b205bf6913b48157c70dc4a1336fe71865
arianrah <arianrahimi@icloud.com>
2021-04-14 20:28:10 -0700

    Completed updateUserProfile action.

--- frontend/src/screens/ProfileScreen.js
+++ frontend/src/screens/ProfileScreen.js
@@ -3,7 +3,7 @@ import { Form, Button, Row, Col } from 'react-bootstrap'
 import { useDispatch, useSelector } from 'react-redux'
 import Message from '../components/Message'
 import Loader from '../components/Loader'
-import { getUserDetails } from '../actions/userActions'
+import { getUserDetails, updateUserProfile } from '../actions/userActions'
 
 const ProfileScreen = ({ location, history }) => {
 	const [name, setName] = useState('')
@@ -20,6 +20,9 @@ const ProfileScreen = ({ location, history }) => {
 	const userLogin = useSelector((state) => state.userLogin)
 	const { userInfo } = userLogin
 
+	const userUpdateProfile = useSelector((state) => state.userUpdateProfile)
+	const { success } = userUpdateProfile
+
 	useEffect(() => {
 		if (!userInfo) {
 			history.push('/login')
@@ -31,13 +34,14 @@ const ProfileScreen = ({ location, history }) => {
 				setEmail(user.email)
 			}
 		}
-	}, [history, userInfo, user])
+	}, [history, userInfo, user, success])
 
 	const submitHandler = (e) => {
 		e.preventDefault()
 		if (password !== confirmPassword) {
 			setMessage('Passwords do not match')
 		} else {
+			dispatch(updateUserProfile({ id: user._id, name, email, password }))
 		}
 	}
 
@@ -48,6 +52,7 @@ const ProfileScreen = ({ location, history }) => {
 				<h2>User Profile</h2>
 				{message && <Message variant="danger">{message}</Message>}
 				{error && <Message variant="danger">{error}</Message>}
+				{success && <Message variant="success">{'Profle Updated'}</Message>}
 				{loading && <Loader />}
 				<Form onSubmit={submitHandler}>
 					<Form.Group controlId="name">
@@ -83,7 +88,7 @@ const ProfileScreen = ({ location, history }) => {
 					<Form.Group controlId="confirmPassword">
 						<Form.Label>Confirm Password</Form.Label>
 						<Form.Control
-							type="confirmPassword"
+							type="password"
 							placeholder="Confirm password"
 							value={confirmPassword}
 							onChange={(e) => setConfirmPassword(e.target.value)}
